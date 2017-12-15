aebd4176ff7894c6b9156cb0042c92a57e9ac418
vinay mavi <vinaymavi@gmail.com>
2017-12-15 16:29:16 +0530

    NavLink used for applying activeClass and activeStyle

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import Hello from "./Hello";
 import About from "./About";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link,NavLink } from "react-router-dom";
 const Links = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>      
+      <NavLink activeStyle={{color:'red'}} to="/">Home</NavLink>
+      <NavLink activeStyle={{ color: 'red' }} to="/about">About</NavLink>      
     </nav>
   );
 };
