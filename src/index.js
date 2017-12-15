b657db9a324bc0b7c95e9fd467aae5b7aca92f04
vinay mavi <vinaymavi@gmail.com>
2017-12-15 16:20:58 +0530

    Link Component used for navigation.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import { render } from "react-dom";
 import Hello from "./Hello";
 import About from "./About";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+const Links = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>      
+    </nav>
+  );
+};
 const App = () => (
   <Router>
     <div>
+      <Links/>
       <Route exact path="/" component={Hello} />
       <Route path="/about" component={About} />
     </div>
